598fb8506b925d484589e65697c0fcbecb6df04d
Zubo Gu <gu.zubo@berkeley.edu>
2018-01-09 10:22:27 -0800

    Revert "Update main.js"
    This reverts commit 8c4627a5f2c7f9bfbc57fc7fbd44d67cd6b9f8cd.

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,26 +3,7 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here}
-    if (answer == '' || attempt == '') {
-        setHiddenFields();
-    }
-}
-
-function setHiddenFields() {
-    var answer = Math.floor(Math.random() * 10000).toString();
-    while (answer.length < 4) {
-        if (answer.length == 1) {
-            answer = "000"+ answer;
-        }
-        if (answer.length == 2) {
-            answer = "00"+ answer;
-        }
-        if (answer.length == 3) {
-            answer = "0"+ answer;
-        }
-    }
-    attempt = 0;
+    //add functionality to guess function here
 }
 
 //implement new functions here
